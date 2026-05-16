2a97723e80415632add4b0e7c411b7ee2904b6d5
Milan Gayen <milangayenbuie@gmail.com>
2026-05-16 09:56:52 +0530

    Task Completed CSS Fixes

--- class5/main.js
+++ class5/main.js
@@ -45,7 +45,7 @@ function render() {
     for (let i = 0; i < todo.length; i++) {
         tasklistwrapper.innerHTML += `
          <div class='listWrapper max-w-full flex justify-between items-center mx-3 my-3 bg-white px-3 py-3'>
-            <div class='${todo[i].isCompleted ? "line-through" : ""}'>${todo[i].taskName}</div>
+            <div class='${todo[i].isCompleted ? "line-through disabled" : ""}'>${todo[i].taskName}</div>
             <div class='ctaButtonWrapper flex gap-1'>
                 <button onClick='doneTodo(${i})' class='checkButtom text-white bg-success hover:bg-success-strong cursor-pointer font-medium leading-5 rounded-full text-sm px-4 py-2.5'>Complete</button>
                 <button onClick='deleteTodo(${i})' class='delButtom text-white bg-danger hover:bg-danger-strong cursor-pointer font-medium leading-5 rounded-full text-sm px-4 py-2.5'>Delete</button>
